1e9cdcebd5aebac5930828f8f3c06530d05cfd44
Chakravarth <chakrijallu7@gmail.com>
2023-11-01 20:48:41 +0530

    second commit

--- src/controller/dbController.js
+++ src/controller/dbController.js
@@ -1,6 +1,6 @@
 let mongo = require("mongodb");
 let MongoClient = mongo.MongoClient;
-let mongoUrl = "mongodb://127.0.0.1:27017"
+let mongoUrl = "mongodb+srv://chakrijallu7:chakrijallu7@react-cluster.7fmkr2r.mongodb.net/fullstack-app"
 let db;
 
 function dbConnect() {
